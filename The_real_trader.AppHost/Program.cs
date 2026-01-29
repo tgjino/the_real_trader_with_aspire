2a176fb0e24124cfe71f12b0a057c0a446e7d5b6
tgjino <108174360+tgjino@users.noreply.github.com>
2026-01-29 07:15:11 +0000

    program.cs file configuared for database

--- The_real_trader.AppHost/Program.cs
+++ The_real_trader.AppHost/Program.cs
@@ -3,7 +3,14 @@ using Aspire.Hosting.Python;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var tradingService = builder.AddPythonProject("trading-service", "../trading_service", "api_app.py")
-                            .WithExternalHttpEndpoints(port: 5000, name: "http");
+                            .WithExternalHttpEndpoints(port: 5000, name: "http")
+                            .WithEnvironment("PORT", "5000")
+                            .WithEnvironment("client_id", builder.Configuration["Fyers:ClientId"])
+                            .WithEnvironment("secret_key", builder.Configuration["Fyers:SecretKey"])
+                            .WithEnvironment("redirect_uri", builder.Configuration["Fyers:RedirectUri"])
+                            .WithDataVolume("trading_storage", "/app/data")
+                            .WithEnvironment("DB_PATH", "/app/data/trading.db");
+                            
 var apiService = builder.AddProject<Projects.The_real_trader_ApiService>("apiservice");
 
 builder.AddProject<Projects.The_real_trader_Web>("webfrontend")
