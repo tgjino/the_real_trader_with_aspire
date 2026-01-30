8517f73d2c4d9e7e9a1b4f6e947d0936c187f95e
tgjino <108174360+tgjino@users.noreply.github.com>
2026-01-30 11:01:13 +0000

    program.cs file error fixed

--- The_real_trader.AppHost/Program.cs
+++ The_real_trader.AppHost/Program.cs
@@ -1,15 +1,15 @@
-
+using Aspire.Hosting;
 using Aspire.Hosting.Python;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var tradingService = builder.AddPythonProject("trading-service", "../trading_service", "api_app.py")
-                            .WithExternalHttpEndpoints(port: 5000, name: "http")
-                            .WithEnvironment("PORT", "5000")
+                            .WithHttpEndpoint(targetPort: 5000)
+                            .WithExternalHttpEndpoints()
                             .WithEnvironment("client_id", builder.Configuration["Fyers:ClientId"])
                             .WithEnvironment("secret_key", builder.Configuration["Fyers:SecretKey"])
                             .WithEnvironment("redirect_uri", "https://studious-spoon-v9p5xpvvwq53wxvw-5000.app.github.dev/callback")
-                            .WithDataVolume("trading_storage", "/app/data")
-                            .WithEnvironment("DB_PATH", "/app/data/trading.db");
+                            // .WithDataVolume("trading_storage", "/app/data")
+                            .WithEnvironment("DB_PATH", "trading.db");
                             
 var apiService = builder.AddProject<Projects.The_real_trader_ApiService>("apiservice");
 
